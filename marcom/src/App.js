4141119ca1cabf43853690858fdd12dd4332db41
Mohit Sharma <119623899+sindhaya@users.noreply.github.com>
2024-07-04 15:29:23 +0530

    react app

---
+++ marcom/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <text>MARCOM USA</text>
+      </header>
+    </div>
+  );
+}
+
+export default App;
